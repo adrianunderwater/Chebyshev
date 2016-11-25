a32e7c8b7fbb2e0093480aa8129c1e7e195bd983
Adrian <adrian.corfield@oceansignal.com>
2016-11-25 14:57:05 +0000

    Initial source commit

---
+++ ChebyshevForm/Chebyshev.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChebyshevForm
+{
+    class Chebyshev
+    {
+        public int N;                      
+        public double[] coefs;
+        public double start, end;
+        public Func<double, double> function;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="n">Number of coefficients to be used.</param>
+        /// <param name="start">start of the domain</param>
+        /// <param name="end">end of the domain</param>
+        /// <param name="function">function to approximate.</param>
+        public Chebyshev(int n, double start, double end, Func<double, double> function)
+        {
+            N = n;
+            this.start = start;
+            this.end = end;
+            this.function = function;
+
+            createCoef();
+        }
+
+        /// <summary>
+        /// Creates the coefficients for the function approximation.
+        /// </summary>
+        private void createCoef()
+        {
+            coefs = new double[N];
+
+            for (int j = 0; j < N; j++)
+            {
+                coefs[j] = 0;
+
+                for (int k = 1; k <= N; k++)
+                {
+                    coefs[j] += function(scaleFromUnity(Math.Cos(Math.PI * (k - 0.5) / N))) * Math.Cos(Math.PI * j * (k - 0.5) / N);
+                }
+
+                coefs[j] = coefs[j] * 2 / N;
+            }
+        }
+
+        /// <summary>
+        /// Evaluation of Chebicgev polybomials for the Chebichev nodes.
+        /// Not used for now...
+        /// </summary>
+        /// <param name="j"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        private double Tn(int j, int k)
+        {
+            return Math.Cos(Math.PI * j * (k - 0.5) / N);
+        }
+
+        /// <summary>
+        /// Recursive version of the evaluation of the Chebychef polynomials.
+        /// </summary>
+        /// <param name="n">Order of polynomail to evaluate</param>
+        /// <param name="x">Value to evaluate on</param>
+        /// <returns>result.</returns>
+        private double Tn_(int n, double x)
+        {
+            double xx = 0;
+
+            switch (n)
+            {
+                case 0:
+                    xx = 1;
+                    break;
+                case 1:
+                    xx = x;
+                    break;
+                default:
+                    xx = 2 * x * Tn_(n - 1, x) - Tn_(n - 2, x);
+                    break;
+            }
+            return xx;
+        }
+
+        /// <summary>
+        /// Iterative version of the evaluation of the Chebychef polynomials.
+        /// </summary>
+        /// <param name="n">Order of polynomail to evaluate</param>
+        /// <param name="x">Value to evaluate on</param>
+        /// <returns>result.</returns>
+        private double Tn(int n, double x)
+        {
+            double x0 = 1;
+            double x1 = x;
+            double xx = 0;
+
+            switch (n)
+            {
+                case 0:
+                    xx = 1;
+                    break;
+                case 1:
+                    xx = x;
+                    break;
+                default:
+                    for (int i = 2; i <= n; i++)
+                    {
+                        xx = 2 * x * x1 - x0;
+                        x0 = x1;
+                        x1 = xx;
+                    }
+                    break;                    
+            }
+            return xx;
+        }
+
+        /// <summary>
+        /// This is used to calculate the coefficients.
+        /// </summary>
+        /// <param name="xx"></param>
+        /// <returns></returns>
+        private double scaleFromUnity(double xx)
+        {
+            return ((start - end) * xx + start + end) / 2;
+        }
+
+        /// <summary>
+        /// Takes the bounds of the functon to evaluate and scales it to [-1, 1]
+        /// </summary>
+        /// This is necessary as the Chebychev approximation only works in [-1,1]
+        /// So any function to be approximated must first be scaled to this domain.
+        /// Note: this means that functions with unbounded domains must undergo
+        /// a transformation to a bounded one before doing this approximation.
+        /// <param name="xx">value to scale</param>
+        /// <returns>scaled value</returns>
+        private double scaleToUnity(double xx)
+        {
+            return (2 * xx - end - start) / (start - end);
+        }
+
+        /// <summary>
+        /// Evaluates the Chebyshev polynomial.
+        /// </summary>
+        /// <param name="x">Value to evaluate</param>
+        /// <returns>he evaluated function</returns>
+        public double evaluate(double x)
+        {
+            double xx = 0;
+
+            x = scaleToUnity(x);
+
+            for (int i = 0; i < N; i++)
+            {
+                xx += coefs[i] * Tn(i, x);
+            }
+
+            xx -= coefs[0] / 2;
+
+            return xx;
+        }
+    }
+}
---
+++ ChebyshevForm/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChebyshevForm
+{
+    public partial class Form1 : Form
+    {
+        Chebyshev cheb;
+        double start, end;  // Start and end of domain to do the approximation on
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            start = 0;
+            end = 0.5;
+        }
+
+        /// <summary>
+        /// Function to approximate
+        /// </summary>
+        /// <param name="xx"></param>
+        /// <returns></returns>
+        private double function(double xx)
+        {
+            //return Math.Exp(xx);
+            return Math.Acos(xx);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            double x1, x2, error;
+
+            if (int.TryParse(textBox1.Text.Trim(), out n))
+            {
+                cheb = new Chebyshev(n, start, end, function);
+            }
+
+            StringBuilder strb = new StringBuilder();
+
+            error = 0;
+
+            for (double xx = start; xx <= end; xx+= (end - start)/10)
+            {
+                x1 = function(xx);
+                x2 = cheb.evaluate(xx);
+                strb.Append(string.Format("{0:f6} {1:f6} {2:f6} {3:f6}\n", xx, x1, x2, Math.Abs(x1 - x2)));
+                error = Math.Max(error, Math.Abs(x1 - x2));
+            }
+            strb.Append(string.Format("Max error = {0}", error));
+
+            strb.Append("\n\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                strb.Append(string.Format("c[{0}] = {1}\n", i, cheb.coefs[i]));
+            }
+
+            strb.Append(string.Format("\nstart = {0};\n", start));
+            strb.Append(string.Format("end = {0};\n", end));
+            strb.Append("\n\n");
+
+            strb.Append(string.Format("double func(double x)\n{{\n"));
+            strb.Append(string.Format("    int i;\n"));
+            strb.Append(string.Format("    double y, z;\n\n"));
+            strb.Append(string.Format("    y = (2 * x - end - start) / (start - end);\n"));
+            strb.Append(string.Format("    z = 0;\n\n"));
+            strb.Append(string.Format("    for (i = 0; i < {0}; i++)\n", n));
+            strb.Append(string.Format("        z += c[i] * Tn(i, y);\n\n"));
+            strb.Append(string.Format("    z -= c[0] / 2;\n", cheb.coefs[0]));
+            strb.Append(string.Format("    return z\n"));
+            strb.Append(string.Format("}}\n"));
+
+            strb.Append("\n\n");
+            strb.Append("private double Tn(int n, double x)\n");
+            strb.Append("{\n");
+            strb.Append("    double x0 = 1;\n");
+            strb.Append("    double x1 = x;\n");
+            strb.Append("    double xx = 0;\n");
+            strb.Append("    \n");
+            strb.Append("    switch (n)\n");
+            strb.Append("    {\n");
+            strb.Append("        case 0:\n");
+            strb.Append("            return 1;\n");
+            strb.Append("        case 1:\n");
+            strb.Append("            return x;\n");
+            strb.Append("        default:\n");
+            strb.Append("            for (int i = 2; i <= n; i++)\n");
+            strb.Append("            {\n");
+            strb.Append("                xx = 2 * x * x1 - x0;\n");
+            strb.Append("                x0 = x1;\n");
+            strb.Append("                x1 = xx;\n");
+            strb.Append("            }\n");
+            strb.Append("            return xx;\n");
+            strb.Append("    }\n");
+            strb.Append("}\n");
+
+
+
+            richTextBox1.Text = strb.ToString();
+        }
+    }
+}
